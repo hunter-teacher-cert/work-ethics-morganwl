840908f97c192938473e04b56118dfd5f93ae448
Morgan Wajda-Levie (laptop) <morgan.wajdalevie@gmail.com>
2021-12-15 19:18:18 -0500

    added week 7 regex exercise

--- week_07_regex/regex.py
+++ week_07_regex/regex.py
@@ -6,11 +6,18 @@
 # dictionaries or lists of common names, though what do you do for names
 # that are common words? anyhow.
 
+import argparse
 import re
 import sys
 
 include_pattern = None
 exclude_pattern = None
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file',
+                        help=('file to search for names'))
+    return vars(parser.parse_args())
 
 def build_patterns():
     build_include()
@@ -23,12 +30,12 @@ def build_include():
 
     # building blocks
     name = r'[A-Z][a-z]+'
-    honorific = r'\.|'.join(['Mr', 'Ms', 'Mrs', 'Dr'])
+    honorific = r'\.|'.join(['Mr', 'Ms', 'Mrs', 'Dr', 'Mx'])
 
     # first name, last name
-    patterns.append(f'(?:{name} {name})')
+    patterns.append(r'(?:{} {})'.format(name, name))
     # honorific, last name
-    patterns.append(f'(?:(?:{honorific}) {name})')
+    patterns.append(r'(?:(?:{}) {})'.format(honorific, name))
 
     include_pattern = re.compile('|'.join(patterns))
 
@@ -37,12 +44,16 @@ def build_exclude():
     global exclude_pattern
     patterns = []
 
-    #building blocks
-    articles = r'|'.join([r'\bThe\b', '\bA\b'])
-    
-    patterns.append(f'(?:{articles})')
+    # stop-words
+    stopwords = ['the', 'a', 'on', 'in', 'under']
+    stopwords = ['\b{}\b'.format(''.join('[{}{}]'.format(letter.upper(),
+                                                         letter.lower())
+                                         for letter in word)) for word
+                 in stopwords]
+    patterns.append('(?:{})'.format('|'.join(stopwords)))
     
     exclude_pattern = re.compile('|'.join(patterns))
+    print(exclude_pattern)
 
 def find_names(s):
     """Return a set of all names in a string."""
@@ -50,15 +61,28 @@ def find_names(s):
     found = (name for name in found if not exclude_pattern.match(name))
     return found
 
-def main(infile):
+
+def find_names_2(s):
+    """Return a set of all names in a string."""
+
+    # Replace stop-words with a special character
+    # This keeps patterns from matching across removed stopwords
+    s = re.sub(exclude_pattern, '%%%', s)
+    print(s)
+
+    # Run super-pattern!
+    found = set(m.group() for m in include_pattern.finditer(s))
+    return found
+
+def main(input_file):
     """Read from infile, search from names, and print them."""
     build_patterns()
     names = set()
-    with open(infile) as fh:
+    with open(input_file) as fh:
         for line in fh:
-            names.update(find_names(line))
+            names.update(find_names_2(line))
     print('\n'.join(sorted(names)))
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(**parse_args())
